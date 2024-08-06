2e65f29d911ff6a30170422d48ab87c1750508d7
karimfifa <fifakarim52@gmail.com>
2024-08-06 13:06:13 +0200

    update page.tsx

---
+++ app/tstp/page.tsx
@@ -0,0 +1,17 @@
+'use client'
+import Image from "next/image";
+import AboutPage from "@/components/component/about-page";
+import { useEffect, useState } from "react";
+export default function Home() {
+
+
+
+  return (
+    <>
+    
+    
+    <h1>im a page from shark 8</h1>
+    </>
+   
+  );
+}
